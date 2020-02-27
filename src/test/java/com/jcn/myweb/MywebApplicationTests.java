3dd7b2d436168684ddda085ab29767d4a63c3484
jianzhu <987639660@qq.com>
2020-02-27 14:32:58 +0800

    update

---
+++ src/test/java/com/jcn/myweb/MywebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jcn.myweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MywebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
